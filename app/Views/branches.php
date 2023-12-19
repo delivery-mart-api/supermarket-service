1b1a9e8381980eb8edd05542b547c54560135c91
IvanLeovandi <ivanleovandi@gmail.com>
2023-12-19 15:30:41 +0700

    add validation interface for branches

--- app/Views/branches.php
+++ app/Views/branches.php
@@ -6,15 +6,19 @@
             <h2 class="text-primary">Daftar Cabang</h2>
         </div>
         <div class="d-flex flex-wrap p-3">
-        <?php foreach($branches as $branch) : ?>
-            <!-- branch card -->
-            <div class="card p-2 mx-2 my-4" style="width: 18rem">
-                <img class="card-img-top" src="https://awsimages.detik.net.id/community/media/visual/2019/11/28/782baf48-5106-4de3-a06a-74d4346a84a9_169.jpeg?w=1200" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title text-center"><?= $branch['name']?></h5>
-                </div>
-            </div>
-        <?php endforeach; ?>
+            <?php if (empty($branches)) : ?>
+                <p class="text-center text-body-secondary fs-5">Belum ada cabang yang terdaftar</p>
+            <?php else : ?>            
+                <?php foreach($branches as $branch) : ?>
+                    <!-- branch card -->
+                    <div class="card p-2 mx-2 my-4" style="width: 18rem">
+                        <img class="card-img-top" src="https://awsimages.detik.net.id/community/media/visual/2019/11/28/782baf48-5106-4de3-a06a-74d4346a84a9_169.jpeg?w=1200" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title text-center"><?= $branch['name']?></h5>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 <?= $this->endSection(); ?>
